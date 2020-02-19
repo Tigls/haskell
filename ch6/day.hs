5bb8cbc395cdb2cd3cc691083b1ddac7bc4fced2
Dima Khort <dima.khort@eonaura.de>
2020-02-19 19:56:26 +0200

    Completed chapter 6

--- ch6/day.hs
+++ ch6/day.hs
@@ -1,4 +1,12 @@
 
 module Day where
-  data DayOfWeek = Mon | Tue | Weds | Thu | Fri | Sat | Sun deriving (Ord, Show)
-
+  data DayOfWeek = Mon | Tue | Weds | Thu | Fri | Sat | Sun deriving ( Ord, Show)
+  instance Eq DayOfWeek where
+    (==) Mon Mon = True
+    (==) Tue Tue = True
+    (==) Weds Weds = True
+    (==) Thu Thu = True
+    (==) Fri Fri = True
+    (==) Sat Sat = True
+    (==) Sun Sun = True
+    (==) _ _ = False
---
+++ ch6/matchTypes.hs
@@ -0,0 +1,6 @@
+import Data.List
+
+myX = 1 :: Int
+
+sigmund :: Int -> Int
+sigmund x = myX
